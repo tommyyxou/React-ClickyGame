8c6208c6d2ed09d664a05c2f63c51a0fa53d3dc7
Tommy Ou <Tommy.yx.ou@gmail.com>
2019-06-12 12:49:21 -0700

    final

--- src/components/scoreBoard.js
+++ src/components/scoreBoard.js
@@ -6,8 +6,8 @@ function ScoreBoard (props) {
     return(
         <div className="scoreStyle">
             <div className="row">
-                <span className="col-4">Monster Hunter Memory Game</span>
-                <span className="col-4">{props.statusMsg}</span>
+                <span className="col-3">Monster Hunter Memory Game</span>
+                <span className="col-5">{props.statusMsg}</span>
                 <span className="col-2">The Score is {props.score}</span>
                 <span className="col-2">The High Score is {props.highScore}</span>
             </div>
